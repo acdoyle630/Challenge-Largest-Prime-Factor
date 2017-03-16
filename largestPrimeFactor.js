44eba527ee57f4202d595ad869e321bd27b8d3a1
Adam <acdoyle630@gmail.com>
2017-03-16 09:49:49 -1000

    prime Factors

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,20 @@
-exports.largestPrimeFactor = function(n){
+function largestPrimeFactor(n){
   var primeNumber = 0;
-  // do your work here
-
-  return primeNumber;
-};
+  var prime = [1,2];
+  var factors = [];
+  for (var i=3; i<=n; i++) {
+  	for (var j=2; j<n[i]; j++) {
+  		}
+  		if(i % j !==0) {
+  		prime.push(i);
+  	 }
+  }
+  for (var t=0; t<prime.length; t++) {
+  	if (n % prime[t] === 0) {
+  		factors.push(prime[t]);
+  	}
+  }
+ return factors; 
+}
+console.log (largestPrimeFactor(10));
+  
